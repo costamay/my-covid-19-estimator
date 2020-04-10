c27e5f3d8bb714aee6d14fd4a6805403bf341927
moringa <costamay>
2020-04-10 09:58:23 +0300

    Add changes

--- src/estimator.py
+++ src/estimator.py
@@ -12,13 +12,13 @@ def estimator(data):
           'currentlyInfected':  impactdata(data),
           'infectionsByRequestedTime': impactdata(data) * (2 ** infectionsByRequestedTime(data)),
           'severeCasesByRequestedTime': (15/100 * (impactdata(data) * (2 ** infectionsByRequestedTime(data)))),
-          'hospitalBedsByRequestedTime': math.floor((35/100 * (int(data['totalHospitalBeds']))) -((15/100(impactdata(data))) * (2 ** infectionsByRequestedTime(data))))
+          'hospitalBedsByRequestedTime': math.floor((35/100 * ((data['totalHospitalBeds']))) -((15/100 * (impactdata(data))) * (2 ** infectionsByRequestedTime(data))))
             }, 
         "severeImpact": {
           'currentlyInfected': severe(data),
           'infectionsByRequestedTime': severe(data) * (2 ** infectionsByRequestedTime(data)),
           'severeCasesByRequestedTime': 15/100 * (severe(data) * (2 ** infectionsByRequestedTime(data))),
-          'hospitalBedsByRequestedTime': math.floor((35/100 * (int(data['totalHospitalBeds']))) -((15/100(severe(data))) * (2 ** infectionsByRequestedTime(data))))
+          'hospitalBedsByRequestedTime': math.floor((35/100 * ((data['totalHospitalBeds']))) -((15/100 * (severe(data))) * (2 ** infectionsByRequestedTime(data))))
           }
        }
 
