47204af287ec16cec14b439ff94f9386792d7e72
moringa <costamay>
2020-04-10 05:53:04 +0300

    Add changes

--- src/infectionsByRequestedTime.py
+++ src/infectionsByRequestedTime.py
@@ -1,7 +1,7 @@
 import math
 def infectionsByRequestedTime(data):
-    if data['periodType'] is 'days':
+    if data['periodType'] == 'days':
         return math.floor((data['timeToElapse'])/3)
-    elif data['periodType'] is 'weeks':
+    elif data['periodType'] == 'weeks':
         return math.floor((data['timeToElapse'] * 7)/3)
     return math.floor((data['timeToElapse'] * 30)/3)
