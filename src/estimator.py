9aa29c01b9f27e0d516703ec1c272d6b7eef82f6
moringa <costamay>
2020-04-11 17:53:36 +0300

    Add new changes

--- src/estimator.py
+++ src/estimator.py
@@ -1,6 +1,6 @@
 def estimator(data):
-      currentlyInfectedImpact = int(data['reportedCases']) * 10
-      currentlyInfectedSevereImpact = int(data['reportedCases']) * 50
+      currentlyInfectedImpact = int(data['reportedCases'] * 10)
+      currentlyInfectedSevereImpact = int(data['reportedCases'] * 50)
       days = timeEstimate(data)
       
       return {
@@ -18,14 +18,14 @@ def estimator(data):
         }
       }
       
-def timeEstimate(data):
-      if data['periodType'] == 'days':
-            days = data['timeToElapse']
-      elif data['periodType'] == 'weeks':
-            days = data['timeToElapse'] * 7
-      elif data['periodType'] == 'months':
-            days = data['timeToElapse'] * 30
-      return days
+# def timeEstimate(data):
+#       if data['periodType'] == 'days':
+#             days = data['timeToElapse']
+#       elif data['periodType'] == 'weeks':
+#             days = data['timeToElapse'] * 7
+#       elif data['periodType'] == 'months':
+#             days = data['timeToElapse'] * 30
+#       return days
        
       
       
