d465d9f90b891e53e143c4300e88b1734656568d
moringa <costamay>
2020-04-11 17:23:55 +0300

    Add new changes

--- src/estimator.py
+++ src/estimator.py
@@ -1,8 +1,3 @@
-# from .infectionsByRequestedTime import infectionsByRequestedTime
-# from .impact import impactdata
-# from .servereImpact import severe
-# import math
-
 def timeEstimate(data):
       if data['periodType'] == 'days':
             days = data['timeToElapse']
@@ -37,20 +32,3 @@ def estimator(data):
       
       
       
-# def estimator(data):
-#       return {
-#         "data": data,
-#         "impact": {
-#           'currentlyInfected':  impactdata(data),
-#           'infectionsByRequestedTime': impactdata(data) * (2 ** infectionsByRequestedTime(data)),
-#           'severeCasesByRequestedTime': (15/100 * (impactdata(data) * (2 ** infectionsByRequestedTime(data)))),
-#           'hospitalBedsByRequestedTime': math.floor((35/100 * ((data['totalHospitalBeds']))) -((15/100 * (impactdata(data))) * (2 ** infectionsByRequestedTime(data))))
-#             }, 
-#         "severeImpact": {
-#           'currentlyInfected': severe(data),
-#           'infectionsByRequestedTime': severe(data) * (2 ** infectionsByRequestedTime(data)),
-#           'severeCasesByRequestedTime': 15/100 * (severe(data) * (2 ** infectionsByRequestedTime(data))),
-#           'hospitalBedsByRequestedTime': math.floor((35/100 * ((data['totalHospitalBeds']))) -((15/100 * (severe(data))) * (2 ** infectionsByRequestedTime(data))))
-#           }
-#        }
-
