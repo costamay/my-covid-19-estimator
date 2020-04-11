cc716220cef6e59aa901ef5251aece7be52e16ea
moringa <costamay>
2020-04-11 17:57:08 +0300

    Add new changes

--- src/estimator.py
+++ src/estimator.py
@@ -1,3 +1,12 @@
+def timeEstimate(data):
+      if data['periodType'] == 'days':
+            days = data['timeToElapse']
+      elif data['periodType'] == 'weeks':
+            days = data['timeToElapse'] * 7
+      elif data['periodType'] == 'months':
+            days = data['timeToElapse'] * 30
+      return days
+       
 def estimator(data):
       currentlyInfectedImpact = int(data['reportedCases'] * 10)
       currentlyInfectedSevereImpact = int(data['reportedCases'] * 50)
@@ -18,14 +27,6 @@ def estimator(data):
         }
       }
       
-# def timeEstimate(data):
-#       if data['periodType'] == 'days':
-#             days = data['timeToElapse']
-#       elif data['periodType'] == 'weeks':
-#             days = data['timeToElapse'] * 7
-#       elif data['periodType'] == 'months':
-#             days = data['timeToElapse'] * 30
-#       return days
-       
+
       
       
